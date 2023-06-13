deb5518fca4cefacccbfadd5357bf2cda0d69e26
jameschu225 <131737019+jameschu225@users.noreply.github.com>
2023-06-12 22:40:58 -0700

    assignment first jsx

--- first-jsx/src/App.js
+++ first-jsx/src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Hello Dojo!</h1>
+      <p>Things I need to do:</p>
+        <li>Learn React</li>
+        <li>Climb Mt.Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
       </header>
     </div>
   );
